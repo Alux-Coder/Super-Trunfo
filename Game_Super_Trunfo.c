45c5607e3a5c6595a4c6fa910d82fa0c2c49858c
Alexsander Mendonça <141535211+Alux-Coder@users.noreply.github.com>
2025-02-21 20:00:06 -0300

    Create Game_Super_Trunfo.c
    Criando as variáveis

---
+++ Game_Super_Trunfo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  char name[50], name2[50];
+  int population, population2;
+  float area, area2;
+  float PIB, PIB2;
+  int turistc_points, turistc_points2;
+  float Densi_population, Densi_population2;
+}
